5513e2cbb1620503ebb6ed0c86771cb296ffe0e5
farhan <farhanj.mohamed@gmail.com>
2022-12-18 11:43:36 -0800

    added a way to show images

--- src/ProductIndex.jsx
+++ src/ProductIndex.jsx
@@ -1,23 +1,31 @@
+import { useState } from "react";
+
 export function ProductIndex(props) {
+  const [searchFilter, setSearchFilter] = useState("");
+
   return (
     <div className="mt-3 container-fluid">
-      <h1 className="mt-2">All Products</h1>
-      {props.products.map((product) => (
-        <div className="mt-3" key={product.id}>
-          {/* {props.images.map((image) => (
-            <div key={image.id}>
-              <img src={image.url} alt="current image" />
-            </div>
-          ))} */}
-          <h3>{product.name}</h3>
-          <p>{product.price}</p>
-          <p>{product.description}</p>
-          <p>{product.quantity}</p>
-          <button className="mt-1 btn btn-primary" onClick={() => props.onShowProduct(product)}>
-            More info
-          </button>
-        </div>
-      ))}
+      <input
+        type="text"
+        value={searchFilter}
+        onChange={(event) => setSearchFilter(event.target.value)}
+        placeholder="Search"
+        className="mt-3 form-control container"
+      />
+      <h1 className="mt-4">All Products</h1>
+      {props.products
+        .filter((product) => product.name.toLowerCase().includes(searchFilter.toLowerCase()))
+        .map((product) => (
+          <div className="mt-3" key={product.id}>
+            <h3>{product.name}</h3>
+            <p>{product.price}</p>
+            <p>{product.description}</p>
+            <p>{product.quantity}</p>
+            <button className="mt-1 btn btn-primary" onClick={() => props.onShowProduct(product)}>
+              More info
+            </button>
+          </div>
+        ))}
     </div>
   );
 }
